14a3dbb7220aee939d811feb23d69be4677cd557
= <poweski2009@gmail.com>
2024-09-27 23:38:20 +0200

    import fixes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,13 +4,15 @@ function Index() {
   const currentScene = Page((state) => state.current);
   return (
     <>
-      { currentScene ? currentScene : (
+      {currentScene ? (
+        currentScene
+      ) : (
         <div className="main">
           <h1>No current Scene</h1>
         </div>
       )}
     </>
-  )
+  );
 }
 
 export default Index;
---
+++ src/utils/initPlugins.ts
@@ -0,0 +1,5 @@
+/*
+  PRE ELECTRON & REACT STORAGE PLACEHOLDER:
+  If you want your plugin to be used you need to include it here
+*/
+export const found = import.meta.glob("../extensions/*/index.tsx");
