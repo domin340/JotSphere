48ac1e85fc163c7edde0d94b513441b64e8adaa7
DOMIN1310 <96619090+domin340@users.noreply.github.com>
2024-09-27 23:53:00 +0200

    Update devs.ts

--- src/api/devs.ts
+++ src/api/devs.ts
@@ -4,13 +4,13 @@ import { Dev } from "./devs.types";
   While creating an extension you MUST include yourself here as a developer.
   IN THIS OBJECT.
 
-  Even tho it might not make that much sense now i believe it's much easier to include yourself
-  via developers rathered than writing it down each time.
+  Even tho it might not make that much sense now I believe it's much easier to include yourself
+  via this object rather than writing it down each time.
 
-  Object will be checked you shall not have inapropriate name or nickname
+  Object will be checked. You shall not have inapropriate nickname or username
 
   example:
-    nickname/username: Developer
+    nickname: Developer
 */
 
 export const Devs = Object.freeze({
