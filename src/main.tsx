bb5e3877cee3e2c63db27ebe2cc6b9e73f950378
= <poweski2009@gmail.com>
2024-09-28 20:37:44 +0200

    developers db

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import "./styles/globals.scss";
 import { KeyListener } from "./utils/keyHandler";
 import Index from "./pages/Index";
 import Navigation from "./lib/@me/Navigation/component";
+import { Init } from "./api/devs/_db/index";
+
+// Initializes whole developers database, It has to run ONLY once
+Init();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/utils/initPlugins.ts
+++
@@ -1,5 +0,0 @@
-/*
-  PRE ELECTRON & REACT STORAGE PLACEHOLDER:
-  If you want your plugin to be used you need to include it here
-*/
-export const found = import.meta.glob("../extensions/*/index.tsx");
